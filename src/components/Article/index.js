4f184c8d88c3bc8c7188ceccd07befea4be586bb
Weslley Botelho <weslleybotelho@Weslleys-MacBook-Pro.local>
2023-05-25 16:09:08 +0200

    Adding style to posts

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,14 @@ import Post from '../Post';
 
 function Artile() {
   return (
-    <>
+    <section className="container">
       <h1>Article.js</h1>
-      <Post />
-    </>
+      <div className="mt-5 container-posts">
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </section>
   );
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,24 @@
 const Post = () => {
-  return <h1>Posts</h1>;
+  return (
+    <div className="post">
+      <div className="img-post"></div>
+      <h5>Tecnologia</h5>
+      <h3>O guia definitivo sobre o blog</h3>
+      <p>
+        Dolore nostrud duis exercitation aute officia velit magna nostrud
+        commodo id duis. Qui eiusmod fugiat magna enim. Officia commodo veniam
+        excepteur fugiat anim do proident ipsum cillum elit tempor sint.
+        Consequat ullamco nulla anim consequat consequat.
+      </p>
+      <div className="flex pt-2">
+        <div className="img-profile"></div>
+        <div className="desc-profile ml-2">
+          <h6 className="color-blue">Fulano de Tal</h6>
+          <p>Aug 2, 2020 - 8 min read</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
