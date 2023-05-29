eb34d050564e3674cd51d76bd7a1fc83678f5085
Weslley Botelho <weslleybotelho@Weslleys-MacBook-Pro.local>
2023-05-29 21:28:11 +0200

    Adding link to Banner and Card

--- src/pages/Home/Banner/index.js
+++ src/pages/Home/Banner/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Banner = ({ content }) => {
   return (
     <>
@@ -7,16 +9,18 @@ const Banner = ({ content }) => {
         </div>
         <div className="row mt-3">
           <h6 className="color-gray text-center">{content.date}</h6>
+
           <h6 className="uppercase color-primary text-center">
             {content.category}
           </h6>
-
-          <h3 className="text-center">{content.title}</h3>
+          <Link to={'/post/' + content.id} className="link-title">
+            <h2 className="text-center">{content.title}</h2>
+          </Link>
           <p className="text-center mt-2">{content.resume}</p>
           <div className="my-3 flex-center">
-            <a href="" className="link color-primary">
+            <Link to={'/post/' + content.id} className="link color-primary">
               Ler mais
-            </a>
+            </Link>
           </div>
         </div>
       </section>
--- src/pages/Home/Card/index.js
+++ src/pages/Home/Card/index.js
@@ -1,23 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const Card = ({ content }) => {
   return (
     <>
-      <div className="grid-4 p-0 card">
+      <div className="grid-4 p-0 card hidden">
         <div className="thumb hidden">
-          <a href="">
+          <Link to={'/post/' + content.id}>
             <img src={content.imageUrl} alt="" />
-          </a>
+          </Link>
         </div>
-        <div className="mt-2">
+        <div className="p-2">
           <h6 className="color-gray">{content.date}</h6>
           <h6 className="uppercase color-primary">{content.category}</h6>
 
-          <h4>{content.title}</h4>
-          <p className="mt-2">{content.resume}</p>
-          <div className="my-3">
-            <a href="" className="link color-primary">
-              Ler mais
-            </a>
-          </div>
+          <Link to={'/post/' + content.id} className="link-title">
+            <h4>{content.title}</h4>
+          </Link>
+          <p className="my-2">{content.resume}</p>
+          <Link to={'/post/' + content.id} className="link color-primary p-0">
+            Ler mais
+          </Link>
         </div>
       </div>
     </>
